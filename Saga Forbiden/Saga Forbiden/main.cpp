acfc5144d791846f245dd6cd9bad9aa393d8582f
prototype-xk <ebaloche@gaming.tech>
2026-01-16 11:15:02 +0100

    Switch Main.cpp

---
+++ Saga Forbiden/Saga Forbiden/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "include/Entities/Player.h"
+#include "include/Core/Application.h"
+#include "include/Game/Game.h"
+
+int main() {
+	Application Setup;
+	Setup.SetupApp();
+
+	return 0;
+}
